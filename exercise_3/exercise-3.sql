a8636c1f1ab69bf9f67940bfa21021c474ea80b8
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-21 12:09:34 -0700

    passing test for excerise two, writing test for 3

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,11 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+SELECT 
+*
+FROM 
+film 
+LEFT JOIN 
+film_category
+ON 
+film_category.film_id = film
