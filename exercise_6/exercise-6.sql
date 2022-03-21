c07164084d3e844a510613c3935f423eb1effd3a
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-21 12:49:28 -0700

    passing test for exercise-6

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,21 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT 
+title 
+FROM
+film
+LEFT JOIN 
+inventory
+ON 
+film.film_id = inventory.film
+_id
+LEFT JOIN
+rental
+ON
+rental.inventory_id = inventory.inventory_id
+LEFT JOIN 
+customer 
+ON 
+customer.customer_id = rental.customer_id
+WHERE 
+customer.first_name = 'Roberta'
